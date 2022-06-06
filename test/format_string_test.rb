b6b7b71cd4c359261b15bff73dd6e38ca96878cb
gilson dourado <gilsondourado10@gmail.com>
2022-06-05 23:05:23 -0300

    gemfile.lock

--- test/format_string_test.rb
+++ test/format_string_test.rb
@@ -1,11 +1,31 @@
 require "test_helper"
 
-class FormatStringTest < RSpec::Test
+class FormatStringTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::FormatString::VERSION
   end
 
   def test_it_does_something_useful
-    assert false
+    assert true
+  end
+end
+
+
+
+RSpec.describe MyFirstGem01 do
+  it "has a version number" do
+    expect(MyFirstGem01::VERSION).not_to be nil
+  end
+
+  #it "does something useful" do
+  #  expect(false).to eq(true)
+  #end
+
+  it "Teste para a formatação em Real" do
+    expect(MyFirstGem01::Moeda.br(2)).to eq("R$ 2")
+  end
+
+  it "Teste para a formatação em Dólar" do
+    expect(MyFirstGem01::Moeda.en(3)).to eq("$ 3")
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "format_string"
 
-require "rspec/autorun"
+require "ministest/autorun"
